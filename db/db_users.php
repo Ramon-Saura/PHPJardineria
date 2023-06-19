1edc9998440bc0b6962b146960f5de1307399650
ramon-saura-dwec <monscave@gmail.com>
2023-06-19 19:18:03 +0200

    start with full calendar

--- db/db_users.php
+++ db/db_users.php
@@ -1,1 +1,16 @@
 <p>esteim alla on toca</p>
+
+<?php
+
+    session_start();
+
+    require($_SERVER['DOCUMENT_ROOT'].'/weave/db_connection.php');
+
+    $sql = 'SELECT * FROM employees';
+
+    $result = mysqli_query($conn, $sql);
+
+    $employees = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    print_r($employees);
+?>
---
+++ view/main.php
@@ -0,0 +1,5 @@
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/weave/components/header.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/weave/components/calendar.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/weave/components/footer.php');
